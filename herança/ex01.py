946ef5e53bdfba9e35593501f4d0865e4eaf0c1f
railandeveloper <128547886+railandeveloper@users.noreply.github.com>
2026-09-10 21:03:00 -0300

    exercicio de heranca

--- herança/ex01.py
+++ herança/ex01.py
@@ -6,15 +6,23 @@ class Pessoa:
     def fazer_aniversario(self):
         self.idade +=1    
         
+    def exibir_dados(self):
+        print(f'Nome: {self.nome} idade:{self.idade}')
+            
+        
 
-class aluno(Pessoa):
+class Aluno(Pessoa):
     def __init__(self, nome, idade, curso, turma):
         super().__init__(nome, idade)
         self.curso = curso
         self.turma = turma  
     
+    def exibir_dados(self):
+        super().exibir_dados()
+        print(f'Curso: {self.curso}, Turma: {self.turma}')
+        
     def fazer_matricula(self):
-        pass
+        print(f'O aluno {self.nome} se matriculou no curso de {self.curso}')
         
         
 class Professor(Pessoa):
@@ -24,7 +32,7 @@ class Professor(Pessoa):
         self.nivel = nivel
         
     def dar_aula(self):
-        pass
+        print(f'professor: {self.nome} iniciou a aula')
  
         
 class Funcionario(Pessoa):
@@ -34,5 +42,19 @@ class Funcionario(Pessoa):
         self.setor = setor
     
     def bater_ponto(self):
-        pass    
+        print(f'{self.nome} acabou de bater o ponto')
+    
+    
+aluno_1 = Aluno('railan', 28, 'ads', 'fundão') 
+aluno_1.fazer_aniversario() 
+aluno_1.fazer_matricula()
+aluno_1.exibir_dados()
+
+profesor_1 = Professor('denis', 42, 'matematica', 'mestrado')
+profesor_1.exibir_dados()
+profesor_1.dar_aula()
+
+funcionario_1 = Funcionario('neymar', 35, 'repositor', 'almoxarifado')
+funcionario_1.exibir_dados()
+funcionario_1.bater_ponto()
                                   
